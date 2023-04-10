4dbb6f63778f2a9e47562515336355d642c303ad
Amin Bandali <bandali@canonical.com>
2023-04-10 08:33:25 -0400

    Add GPLv3 license

--- .github/scripts/check-new-nightly.sh
+++ .github/scripts/check-new-nightly.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# Copyright (C) 2023 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <current_version>"
     exit 1
